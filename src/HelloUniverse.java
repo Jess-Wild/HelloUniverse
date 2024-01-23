9645c0cd9c60fa111fe61c23e1ea238964df3137
Jess-Wild <j.victorion.wcs@gmail.com>
2024-01-23 12:04:10 +0100

    add if/else condition

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,12 +1,24 @@
 public class HelloUniverse {
 
     public static void main(String... args){
-        String phrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
+        /*String phrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
         int nbPlanetes=8;
         System.out.println(phrase + nbPlanetes);
         nbPlanetes++;
         phrase="Il y a quelques années cependant, elles étaient au nombre de : ";
         System.out.println(phrase + nbPlanetes);
+         */
+        
+        String phraseIntroduction = "En %d, les planètes du système solaire étaient au nombre de : %d";
+        int nbPlanetes = 0;
+
+        int annee = 2014;
+        if(annee >= 2006 ){
+            nbPlanetes = 8;
+        }else{
+            nbPlanetes = 9;
+        }
+        System.out.printf(phraseIntroduction, annee, nbPlanetes);
     }
 
 }
