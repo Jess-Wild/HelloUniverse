06ae4058555de288446a14265e93986a9478f3dc
Jess-Wild <j.victorion.wcs@gmail.com>
2024-01-29 15:13:17 +0100

    instances de nouvelles planetes

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -2,6 +2,49 @@ public class HelloUniverse {
 
     public static void main(String... args){
 
+        Planete mercure = new Planete();
+        mercure.nom = "Mercure";
+        mercure.matiere = "Tellurique";
+        mercure.diametre= 4880;
+
+        Planete venus = new Planete();
+        venus.nom = "Venus";
+        venus.matiere = "Tellurique";
+        venus.diametre = 12100;
+
+        Planete terre = new Planete();
+        terre.nom = "Terre";
+        terre.matiere = "Tellurique";
+        terre.diametre = 12756;
+
+        Planete mars = new Planete();
+        mars.nom = "Mars";
+        mars.matiere = "Tellurique";
+        mars.diametre = 6792;
+
+        Planete jupiter = new Planete();
+        jupiter.nom = "Jupiter";
+        jupiter.matiere = "Gazeuse";
+        jupiter.diametre = 142984;
+
+        Planete saturne = new Planete();
+        saturne.nom = "Saturne";
+        saturne.matiere = "Gazeuse";
+        saturne.diametre = 120536;
+
+        Planete uranus = new Planete();
+        uranus.nom = "Uranus";
+        uranus.matiere = "Gazeuse";
+        uranus.diametre = 51118;
+
+        Planete neptune = new Planete();
+        neptune.nom = "Neptune";
+        neptune.matiere = "Gazeuse";
+        neptune.diametre = 49532;
+
+        System.out.println(jupiter.nom + " est une planète " + jupiter.matiere + " avec un diamètre de " + jupiter.diametre + " kilomètres.");
+        
+
         int nbPlanetes = 7;
 
         while (nbPlanetes<10) {
